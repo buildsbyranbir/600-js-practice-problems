a0fd61e9b2f3f5b7ebd5eec147f2b37594d7b1b2
Ranbir <ranbir.webdev@gmail.com>
2026-02-09 10:51:50 +0600

    add part_02.js file

---
+++ chapter_03/part_02.js
@@ -0,0 +1,39 @@
+//problem_01
+let age = 22;
+age = 23;
+console.log(age);
+
+// problem_02
+let pocketMoney = 500;
+pocketMoney = 150;
+console.log(pocketMoney);
+
+// problem_03
+let studyTime = 5;
+studyTime = 3;
+console.log(studyTime);
+
+// problem_04
+let friendsCount = 3;
+friendsCount = 1;
+console.log(friendsCount);
+
+
+// problem_05
+let phonePrice = 25000;
+phonePrice = 12000;
+console.log(phonePrice);
+
+
+// prblem_06
+let chairsCount = 40;
+chairsCount = chairsCount - 2;
+chairsCount = chairsCount - 6;
+console.log(chairsCount);
+
+
+
+// problem_07
+let playTime = 3;
+playTime = 5;
+console.log(playTime);
