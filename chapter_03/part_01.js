94cff10b3e1ae048a079dababf5ce7364c438955
Ranbir <ranbir.webdev@gmail.com>
2026-02-09 10:15:57 +0600

    start my new journey

---
+++ chapter_03/part_01.js
@@ -0,0 +1,25 @@
+// problem_01
+const price = 555;
+console.log(typeof price);
+
+// problem_02
+const learning = true;
+console.log(typeof learning);
+
+// problem_03
+const megaCity = "Dhaka";
+console.log(typeof megaCity);
+
+
+// problem_04
+const temperature = 102.5;
+console.log(typeof temperature);
+
+
+// problem_05
+const isHungry = false;
+console.log(typeof isHungry);
+
+// problem_06
+const isRich = true;
+console.log(typeof isRich);
