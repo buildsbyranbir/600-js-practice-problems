040371f7588318c89ce7e331b33a0dbad6dbb177
Ranbir <ranbir.webdev@gmail.com>
2026-02-15 23:10:03 +0600

    add part_04.js file

---
+++ chapter_03/part_04.js
@@ -0,0 +1,36 @@
+// problem_01
+let favouriteFood = "Pizza";
+
+// problem_02
+let city ="Chittagong"
+let City = "Dhaka"
+console.log(city);
+console.log(City);
+
+// problem_03
+let userName = "John Doe";
+console.log(userName);
+let username = "Jovan";
+console.log(username);
+
+// problem_04
+let homeAddress = "Sahapur,Kaharol,Dinajpur"
+console.log(homeAddress)
+
+// problem_05
+// let super = 100; //super akti js er researved keyword
+// console.log(super);
+let superRun = 100;
+console.log(superRun);
+
+
+// problem_06
+// let class = "Ten";  //class akti js er researved keyword so seta dile error dibe
+// console.log(class);
+let myClass = "Ten";
+console.log(myClass);
+
+// problem_07
+// let 2ndPosition = "Tamim";   //error dibe karon variable er name number diye shuru hoy na
+let secondPosition = "Tamim";
+console.log(secondPosition);
