b67407d696ffa2cae778a9287eb9736f9a0f599e
Ranbir <ranbir.webdev@gmail.com>
2026-02-18 23:24:47 +0600

    add part_05.js

---
+++ chapter_03/part_05.js
@@ -0,0 +1,37 @@
+//problem_01
+let num = "20";
+let number = Number(num) + 10;
+console.log(number); // 30
+
+//problem_02
+let value = '3.14159';
+let integerValue = parseInt(value);
+console.log(integerValue); // 3
+
+// proble_03
+let text = 'premikBoy';
+let result = Number(text);
+console.log(result); // NaN
+
+// problem_04
+let sum = 3.456 + 2.789;
+console.log(sum.toFixed(2)); // 6.25
+
+
+// problem_05
+let num1 = parseFloat('56.78');
+let num2 = parseFloat('12.34');
+let total = num1 + num2;
+console.log(total); // 69.12
+
+
+// problem_06
+let numm = 10.5678 + 5.4321;
+console.log(numm.toFixed(1)); // 16.0
+
+// problem_07
+let newnumber = 42.45689754;
+console.log(newnumber.toFixed(3)); // 42.457
+
+
+
