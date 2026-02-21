10a03a16d82ea31e756fb223276e89f3a9eca96e
Ranbir <ranbir.webdev@gmail.com>
2026-02-21 09:05:31 +0600

    Add chapter 04 to part-01.js

---
+++ chapter-04/part-01.js
@@ -0,0 +1,76 @@
+//problem-01
+let ranbir = 7;
+let topu = 5;
+if (ranbir > topu) {
+    console.log("Ranbir ate more food");
+} else {
+    console.log("Topu ate more food");
+}
+
+
+//problem-02
+let myClass = 45;
+let besiteClass = 50;
+if (myClass > besiteClass) {
+    console.log("My class has more students");
+} else {
+    console.log("Beside class has more students");
+}
+
+
+//problem-03
+let rahimMark =75;
+let karimMark =75;
+if (rahimMark === karimMark){
+    console.log("Both have the same number")
+}
+else{
+    console.log("Both numbers are not equal.")
+}
+
+
+//problem-04
+let target = 10;
+let score = 12;
+
+if (score >= target) {
+    console.log("Target met");
+} else {
+    console.log("Target not met");
+}
+
+
+
+//problem-05
+let a = 10;
+let b = 7;
+
+if (a !== b) {
+  console.log("The two numbers are not equal.");
+} else {
+  console.log("The two numbers are equal.");
+}
+
+
+
+//problem-06
+let num1 = 20;
+let num2 = 15;
+
+if (num1 <= num2) {
+  console.log("20 is less than or equal to 15");
+} else {
+  console.log("20 is greater than 15");
+}
+
+
+
+//problem-07
+let yourMark = 45;
+let friendMark = 97;
+
+if (friendMark < yourMark) {
+  console.log("Your friend scored less than you");
+} else {
+  console.log("Your friend scored more than you");
+}
