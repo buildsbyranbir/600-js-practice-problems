9005a181398bb8c740166271d94f9c443368bdad
Ranbir <ranbir.webdev@gmail.com>
2026-02-22 16:15:08 +0600

    add chapter 04 to part-02

---
+++ chapter-04/part-02.js
@@ -0,0 +1,84 @@
+//problem-01
+let yourAge = 15;
+let brotherAge = 10;
+
+if (brotherAge <= yourAge) {
+  console.log("Your younger brother's age is less than or equal to yours.");
+} else {
+  console.log("Your younger brother is older than you.");
+}
+
+//problem-02
+let passMark = 33;
+let yourMark = 45;
+
+if (yourMark >= passMark) {
+  console.log("You have passed the exam.");
+} else {
+  console.log("You have failed the exam.");
+}
+
+
+//problem-03
+let jamburaPrice = 50;
+let kodbelPrice = 60;
+
+if (jamburaPrice < kodbelPrice) {
+  console.log("Jambura is cheaper than kodbel.");
+} else {
+  console.log("Jambura is not cheaper.");
+}
+
+
+//problem-04
+let yourBooks = 10;
+let friendBooks = 10;
+
+if (yourBooks === friendBooks) {
+  console.log("You both have read the same number of books.");
+} else {
+  console.log("You have not read the same number of books.");
+}
+
+
+//problem-05
+let yourBiscuits = 15;
+let friendBiscuits = 20;
+
+if (yourBiscuits < friendBiscuits) {
+  console.log("You received fewer biscuits than your friend.");
+} else {
+  console.log("You did not receive fewer biscuits.");
+}
+
+
+//problem-06
+let realMadridGoals = 2;
+let barcaGoals = 3;
+
+if (realMadridGoals < barcaGoals) {
+  console.log("Real Madrid scored fewer goals.");
+} else {
+  console.log("Real Madrid did not score fewer goals.");
+}
+
+//problem-07
+let yourName = "Rahim";
+let friendName = "Karim";
+
+if (yourName === friendName) {
+  console.log("The names are the same.");
+} else {
+  console.log("The names are not the same.");
+}
+
+
+//problem-08
+let youDidStudy = true;
+let friendDidStudy = false;
+
+if (youDidStudy === friendDidStudy) {
+  console.log("You both did the same activity.");
+} else {
+  console.log("You did not do the same activity.");
+}
