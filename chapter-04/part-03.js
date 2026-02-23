c2169a591ceadd3ef9c2cfe4c52d838122878de1
Ranbir <ranbir.webdev@gmail.com>
2026-02-23 10:46:36 +0600

    add chaper-04 of part-03

---
+++ chapter-04/part-03.js
@@ -0,0 +1,34 @@
+//problem-01
+console.log(50 == "50");
+console.log(50 === "50");
+
+//problem-02
+console.log("JavaScript" == "JavaScript");
+console.log("JavaScript" === "JavaScript");
+
+//problem-03
+console.log(25 == "25");
+console.log(25 === "25");
+
+
+//problem-04
+console.log("apple" == "apple");
+console.log("apple" === "apple");
+
+//problem-05
+console.log("test" == "TEST");
+console.log("test" === "TEST");
+
+//problem-06
+let variableX = 15;
+let variableY = "20";
+
+console.log(variableX != variableY);
+console.log(variableX !== variableY);
+
+//problem-07
+let variableA = "hello";
+let variableB = "Hello";
+
+console.log(variableA == variableB);
+console.log(variableA === variableB);
