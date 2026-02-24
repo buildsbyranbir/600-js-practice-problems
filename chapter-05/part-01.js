1a5fb57fd478020289f12d848bb48b6e9d689e71
Ranbir <ranbir.webdev@gmail.com>
2026-02-24 07:03:44 +0600

    add chapter-05 of part-01

---
+++ chapter-05/part-01.js
@@ -0,0 +1,58 @@
+//problem-01
+let time = 3; // current time
+
+if (time < 4) {
+  console.log("You will get snacks.");
+} else {
+  console.log("You will be scolded.");
+}
+
+
+//problem-02
+let loginSuccess = true;
+
+if (loginSuccess) {
+  console.log("Welcome!");
+} else {
+  console.log("Get Lost!");
+}
+
+
+//problem-03
+let run = 5;
+
+if (run >= 5) {
+  console.log("You will get chocolate.");
+} else {
+  console.log("You will become fat.");
+}
+
+
+//problem-04
+let marks = 85;
+
+if (marks > 80) {
+  console.log("You will get a bike.");
+} else {
+  console.log("You will not get a bike.");
+}
+
+
+//problem-05
+let showTime = 8;
+
+if (showTime < 9) {
+  console.log("You will watch the movie.");
+} else {
+  console.log("You will sleep.");
+}
+
+
+//problem-06
+let temperature = 32;
+
+if (temperature >= 30) {
+  console.log("Turn on the air conditioner.");
+} else {
+  console.log("Use a blanket and sleep.");
+}
