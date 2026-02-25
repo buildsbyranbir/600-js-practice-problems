bb3097a7df438af86ddd396157110f2624db3bb1
Ranbir <ranbir.webdev@gmail.com>
2026-02-25 22:14:18 +0600

    add chapter-05 of part-02.js file

---
+++ chapter-05/part-02.js
@@ -0,0 +1,97 @@
+// problem-01
+let age = 20;
+let height = 65;
+
+if (age > 18 && height > 60) {
+  console.log("He will drive the car");
+} else {
+  console.log("He will sit in the car");
+}
+
+
+// problem-02
+let math = 75;
+let english = 90;
+
+if (math > 80 || english > 85) {
+  console.log("Eligible for admission.");
+} else {
+  console.log("Not eligible for admission.");
+}
+
+
+// problem-03
+let gpa = 5;
+let income = 8000;
+
+if (gpa === 5 && income < 10000) {
+  console.log("He will get scholarship.");
+} else {
+  console.log("He will study by paying fees.");
+}
+
+
+// problem-04
+let age2 = 28;
+let experience = 3;
+
+if (age2 < 30 && experience > 2) {
+  console.log("Can attend the exam.");
+} else {
+  console.log("Cannot attend the exam.");
+}
+
+
+// problem-05
+let eggs = 15;
+let chicken = false;
+
+if (eggs > 12 || !chicken) {
+  console.log("Cook egg korma.");
+} else {
+  console.log("Cook pulao and banana.");
+}
+
+
+// problem-06
+let temp = 101;
+let cough = true;
+
+if (temp > 100 || cough) {
+  console.log("Go to doctor.");
+} else {
+  console.log("Take rest under blanket.");
+}
+
+
+// problem-07
+let attendance = 85;
+let homework = true;
+
+if (attendance > 80 && homework) {
+  console.log("Allowed to sit in exam.");
+} else {
+  console.log("Auto fail.");
+}
+
+
+// problem-08
+let electricity = false;
+let charge = false;
+
+if (!electricity && !charge) {
+  console.log("Will not study.");
+} else {
+  console.log("Will play video game.");
+}
+
+
+// problem-09
+let price = 1200;
+let coupon = true;
+
+if (price > 1000 && coupon) {
+  console.log("You will get 20% discount.");
+} else {
+  console.log("No discount.");
+}
