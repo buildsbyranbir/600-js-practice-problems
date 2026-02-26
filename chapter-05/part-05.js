ad0eba2529cfec97581e9b7646aa80c6bc442ee4
Ranbir <ranbir.webdev@gmail.com>
2026-02-26 22:30:07 +0600

    add chapter-05 of part-05

---
+++ chapter-05/part-05.js
@@ -0,0 +1,33 @@
+//problem-01
+let amount = 3500;
+let cashback = amount > 3000 ? 500 : 0;
+console.log(cashback);
+
+
+//problem-02
+let age = 16;
+console.log(age > 15 ? "Teenager" : "Child");
+
+//problem-03
+let isLoggedIn = true;
+let message = isLoggedIn ? "Welcome back" : "Please login";
+console.log(message);
+
+
+//problem-04
+let isFull = true;
+console.log(isFull ? "Ready for a long drive" : "Fill the tank");
+
+
+//problem-05
+let passed = false;
+console.log(passed ? "Party time" : "Next semester serious study korbo");
+
+
+//prblem-06
+let sunny = false;
+!sunny && console.log("Stay home");
+
+//problem-07
+let expensive = false;
+!expensive && console.log("I will buy this item.");
