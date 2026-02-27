7751f256484acd438357416a8fb9625f7de3f9ad
Ranbir <ranbir.webdev@gmail.com>
2026-02-27 21:44:24 +0600

    add chapter-06 of part-01

---
+++ chapter-06/part-01.js
@@ -0,0 +1,61 @@
+//problem-01
+let numbers = [];
+
+for (let i = 71; i <= 79; i++) {
+  numbers.push(i);
+}
+
+console.log(numbers);
+
+
+//problem-02
+let fruits = ["Apple", "Banana", "Orange", "Mango", "Guava"];
+
+console.log(fruits);
+
+
+//problem-03
+let vegetables = [
+  "Potato",
+  "Onion",
+  "Eggplant",
+  "Radish",
+  "Carrot",
+  "Tomato",
+  "Taro",
+  "Bottle gourd",
+  "Cucumber",
+  "Pointed gourd"
+];
+
+console.log("Total vegetables:", vegetables.length);
+
+
+//problem-04
+let movies = ["Inception", "Avatar", "Titanic", "3 Idiots", "Interstellar"];
+
+console.log(movies);
+
+
+//problem-05
+let oddNumbers = [];
+
+for (let i = 11; i <= 30; i++) {
+  if (i % 2 !== 0) {
+    oddNumbers.push(i);
+  }
+}
+
+console.log(oddNumbers);
+
+
+//problem-06
+let colors = ["Red", "Green", "Blue", "White", "Black"];
+
+console.log("Total colors:", colors.length);
+
+
+//problem-07
+let capitals = ["Dhaka", "Delhi", "Tokyo", "London", "Paris"];
+
+console.log("Total capitals:", capitals.length);
