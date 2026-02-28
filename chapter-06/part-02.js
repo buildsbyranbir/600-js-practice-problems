f164c06fea2bd5ace0cc17654fb28123df84f300
Ranbir <ranbir.webdev@gmail.com>
2026-02-28 23:00:26 +0600

    add chapter-05 of part-02

---
+++ chapter-06/part-02.js
@@ -0,0 +1,40 @@
+//problem-01
+let numbers = [10, 20, 30, 40, 50];
+
+numbers.push(60);
+
+console.log(numbers);
+
+
+//problem-02
+let friends = ["Sajib", "Sagar", "Sakib", "Sohel"];
+
+friends.push("Suman");
+
+
+
+//problem-03
+let games = ["Free Fire", "PUBG", "Subway Surfers", "Candy Crush"];
+
+games.pop();
+
+console.log(games);
+
+
+//problem-04
+let num = [24, 36, 48, 60];
+
+numbers.unshift(12);
+
+console.log(num);
+
+
+//problem-05
+let books = ["Bangla", "English", "Math", "Physics", "Chemistry"];
+
+books.shift();
+
+console.log(books);
+
+
+//problem-06
