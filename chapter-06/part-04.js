aa46b80123fbe739d3f8163098dd9f950459445e
Ranbir <ranbir.webdev@gmail.com>
2026-03-01 21:51:21 +0600

    add chapter-05 of part-04

---
+++ chapter-06/part-04.js
@@ -0,0 +1,54 @@
+// problem-01
+let fruits = ["Apple", "Banana", "Mango", "Litchi"];
+
+if (fruits.includes("Mango")) {
+  console.log("Mango is available");
+} else {
+  console.log("Mango is not available, climb the tree");
+}
+
+
+// problem-02
+let names = ["Babul", "Alif", "Hosen"];
+
+let babulIndex = names.indexOf("Babul");
+
+console.log("Index number of Babul:", babulIndex);
+
+
+// problem-03
+let friends = ["Rimon", "Rifat", "Rajib"];
+
+let rifatIndex = friends.indexOf("Rifat");
+
+console.log("Index of Rifat:", rifatIndex);
+
+
+// problem-04
+let cities = ["Dhaka", "Chittagong", "Sylhet", "rajshahi"];
+
+if (cities.includes("RajShahi")) {
+  console.log("RajShahi exists");
+} else {
+  console.log("RajShahi does not exist");
+}
+
+
+// problem-05
+let weather = ["Lake", "Cloud", "Rain", "Monsoon"];
+
+if (weather.includes("Rain")) {
+  console.log("I need umbrella");
+} else {
+  console.log("No rain no pain");
+}
+
+
+// problem-06
+let games = ["Football", "Cricket", "Volleyball"];
+
+if (games.includes("Badminton")) {
+  console.log("Badminton is available");
+} else {
+  console.log("Badminton is not available");
+}
