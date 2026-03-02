e6733dcf5ada93e29d2cbcffa6d1ee905a0dd9fb
Ranbir <ranbir.webdev@gmail.com>
2026-03-02 22:58:09 +0600

    add chapter-05 of part-01

---
+++ chapter-07/part-01.js
@@ -0,0 +1,58 @@
+//problem-01
+let subjects = ["Math", "English", "ICT"];
+
+for (let i = 0; i < subjects.length; i++) {
+    console.log(subjects[i]);
+}
+
+
+//problem-02
+let foods = ["Biriyani", "Pizza", "Burger", "Kacchi"];
+
+for (let i = 0; i < foods.length; i++) {
+    console.log(foods[i]);
+}
+
+
+//problem-03
+let birthYears = [1975, 1980, 2002, 2005];
+
+for (let i = 0; i < birthYears.length; i++) {
+    console.log("Birth Year: " + birthYears[i]);
+}
+
+
+//problem-04
+let team = [
+    "Shakib",
+    "Tamim",
+    "Mushfiq",
+    "Riyad",
+    "Litton",
+    "Taskin",
+    "Mustafiz",
+    "Miraz",
+    "Afif",
+    "Hasan",
+    "Ebadot"
+];
+
+for (let i = 0; i < team.length; i++) {
+    console.log(team[i]);
+}
+
+
+//problem-05
+let examDates = [
+    "15 March 2026",
+    "28 March 2026",
+    "10 April 2026",
+    "25 April 2026"
+];
+
+let i = 0;
+
+while (i < examDates.length) {
+    console.log("Exam Date: " + examDates[i]);
+    i++;
+}
