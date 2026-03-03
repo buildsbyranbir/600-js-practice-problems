e21ad7cd46d2e98da66f80534e44f94406f5cf80
Ranbir <ranbir.webdev@gmail.com>
2026-03-03 22:01:10 +0600

    add chapter-05 of part-02

---
+++ chapter-07/part-02.js
@@ -0,0 +1,45 @@
+// problem-01
+let i = 1;
+
+while (i <= 10) {
+  console.log("I will practice programming at least 3 hours every day.");
+  i++;
+}
+
+
+// problem-02
+let num1 = 1;
+
+while (num1 <= 20) {
+  console.log(num1);
+  num1++;
+}
+
+
+// problem-03
+let num2 = 50;
+
+while (num2 <= 100) {
+  console.log(num2);
+  num2++;
+}
+
+
+// problem-04
+let num3 = 1;
+
+while (num3 <= 10) {
+  if (num3 % 3 !== 0) {
+    console.log(num3);
+  }
+  num3++;
+}
+
+
+// problem-05
+let num4 = 111;
+
+while (num4 >= 110) {
+  console.log(num4 / 2);
+  num4--;
+}
