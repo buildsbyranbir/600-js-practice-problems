290b914ab9ab9de2141acfd16d7a7cc8694a000c
Ranbir <ranbir.webdev@gmail.com>
2026-03-08 23:38:15 +0600

    chapter-07 of part-05

---
+++ chapter-07/part-05.js
@@ -0,0 +1,41 @@
+//problem-01
+for (let i = 1; i <= 30; i++) {
+  if (i > 15) break;
+  console.log(i);
+}
+
+
+//problem-02
+for (let i = 1; i <= 40; i++) {
+  if (i % 7 === 0) continue;
+  console.log(i);
+}
+
+
+//problem-03
+for (let i = 1; i <= 15; i++) {
+  if (i === 9) continue;
+  console.log(i);
+}
+
+
+//problem-04
+for (let i = 1; i <= 20; i++) {
+  if (i === 12) continue;
+  console.log(i);
+}
+
+
+//problem-05
+for (let i = 1; i <= 25; i++) {
+  if (i % 3 === 0) continue;
+  console.log(i);
+}
+
+
+
+//problem-06
+for (let i = 91; i <= 120; i++) {
+  if (i % 10 === 0) continue;
+  console.log(i);
+}
