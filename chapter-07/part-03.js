3132dec8055d90040b4efc1cce9dee31641cb679
Ranbir <ranbir.webdev@gmail.com>
2026-03-08 23:10:37 +0600

    add chapter-07 of part-03

---
+++ chapter-07/part-03.js
@@ -0,0 +1,26 @@
+//problem-01
+for (let i = 150; i <= 170; i++) {
+  console.log(i);
+}
+
+
+//problem-02
+let sum = 0;
+
+for (let roll = 31; roll <= 58; roll++) {
+  console.log("Roll:", roll);
+  sum += roll;
+}
+
+console.log("Total Sum =", sum);
+
+
+
+//problem-03
+let total = 0;
+
+for (let num = 25; num <= 75; num++) {
+  total += num;
+}
+
+console.log("Sum =", total);
