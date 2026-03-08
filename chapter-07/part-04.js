73e6056da651204050e566eda592c700f099e751
Ranbir <ranbir.webdev@gmail.com>
2026-03-08 23:35:11 +0600

    add chapter-07 of part-04

---
+++ chapter-07/part-04.js
@@ -0,0 +1,56 @@
+//problem-01
+for (let i = 20; i <= 50; i++) {
+  if (i % 7 === 0) {
+    console.log(i);
+  }
+}
+
+
+//problem-02
+for (let i = 40; i <= 80; i++) {
+  if (i % 5 === 0 && i % 7 === 0) {
+    console.log(i);
+  }
+}
+
+
+
+//problem-03
+let sum = 0;
+
+for (let i = 1; i <= 40; i++) {
+  if (i % 13 === 0) {
+    sum += i;
+  }
+}
+
+console.log("Sum =", sum);
+
+
+
+//problem-04
+for (let i = 1; i <= 50; i += 4) {
+  console.log(i);
+}
+
+
+//problem-05
+for (let i = 0; i <= 100; i++) {
+  if (i % 9 === 0 && i % 6 === 0) {
+    console.log(i);
+  }
+}
+
+
+
+//problem-06
+let total = 0;
+
+for (let i = 1; i <= 50; i++) {
+  if (i % 3 === 0 && i % 4 === 0) {
+    console.log(i);
+    total += i;
+  }
+}
+
+console.log("Sum =", total);
