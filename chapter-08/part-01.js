57f74cecb7685eaffa8c59207dcc98abb5b08732
Ranbir <ranbir.webdev@gmail.com>
2026-03-09 22:39:26 +0600

    add chapter-08 of part-01

---
+++ chapter-08/part-01.js
@@ -0,0 +1,75 @@
+//problem-01
+const teacher = {
+  name: "Rahim Sir",
+  subject: "Mathematics",
+  institute: "Kaliganj Govt. High School",
+  age: 40,
+  address: "Kaliganj, Bangladesh"
+};
+
+
+
+//problem-02
+const tree = {
+  name: "Mango Tree",
+  type: "Fruit Tree",
+  height: "15 feet",
+  age: 10,
+  fruitName: "Mango"
+};
+
+
+
+//problem-03
+const animal = {
+  name: "Cat",
+  color: "White",
+  legs: 4,
+  food: "Fish",
+  sound: "Meow"
+};
+
+
+
+//problem-04
+const father = {
+  name: "Abdul Karim",
+  profession: "Businessman",
+  age: 48,
+  childrenCount: 3,
+  address: "Kaliganj, Bangladesh"
+};
+
+
+
+//problem-05
+const motorbike = {
+  brand: "Yamaha",
+  model: "R15 V4",
+  wheels: 2,
+  color: "Black",
+  topSpeed: "155 km/h",
+  price: "5,50,000 BDT"
+};
+
+
+
+//problem-06
+const bird = {
+  name: "Parrot",
+  color: "Green",
+  food: "Fruits",
+  canFly: true,
+  habitat: "Forest"
+};
+
+
+
+//problem-07
+const laptop = {
+  brand: "HP",
+  processor: "Intel Core i5",
+  ram: "8GB",
+  price: "65,000 BDT",
+  displaySize: "15.6 inch"
+};
