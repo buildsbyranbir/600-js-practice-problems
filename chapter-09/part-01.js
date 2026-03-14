386383ada7d896cde70b9accac49852a600bfa38
Ranbir <ranbir.webdev@gmail.com>
2026-03-14 23:12:36 +0600

    add chapter-09 of part-01

---
+++ chapter-09/part-01.js
@@ -0,0 +1,76 @@
+//problem-01
+function juiceKhao() {
+  console.log("Here is your lemon juice.");
+}
+
+juiceKhao();
+
+
+
+//problem-02
+function myRoutine() {
+  console.log("Wake up from sleep");
+  console.log("Brush teeth");
+  console.log("Wash face");
+  console.log("Eat breakfast");
+  console.log("Start studying");
+}
+
+myRoutine();
+
+
+
+//problem-03
+function myFamily() {
+  console.log("Father");
+  console.log("Mother");
+  console.log("Brother");
+  console.log("Me");
+}
+
+myFamily();
+
+
+
+//problem-04
+function taskDone() {
+  console.log("Studied");
+  console.log("Cleaned my room");
+  console.log("Did exercise");
+  console.log("Practiced coding");
+}
+
+taskDone();
+
+
+//problem-05
+function greetMe() {
+  console.log("Good Morning! Have a great day!");
+}
+
+greetMe();
+
+
+
+//problem-06
+function getFavFriends() {
+  console.log("Rahim");
+  console.log("Karim");
+  console.log("Sohan");
+}
+
+getFavFriends();
+
+
+
+//problem-07
+function introduceMyself() {
+  console.log("Name: Ranbir");
+  console.log("Age: 18");
+  console.log("Address: Bangladesh");
+  console.log("Phone: 0123456789");
+  console.log("Height: 5.7 feet");
+  console.log("Favorite Food: Biryani");
+}
+
+introduceMyself();
