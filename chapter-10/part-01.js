958dbe2fce186b4d0893aa9b163d43c84b3a6876
Ranbir <ranbir.webdev@gmail.com>
2026-05-11 23:21:54 +0600

    add chapter-10 of part-01

---
+++ chapter-10/part-01.js
@@ -0,0 +1,43 @@
+// Problem 1
+function yardToMile(yard) {
+    return yard / 1760;
+}
+
+
+
+// Problem 2
+function kiloWattToCalorie(kwh) {
+    return kwh * 860;
+}
+
+
+
+// Problem 3
+function timeToSecond(hour, minute) {
+    return (hour * 3600) + (minute * 60);
+}
+
+
+
+// Problem 4
+function centimeterToMeter(cm) {
+    return cm / 100;
+}
+
+
+// Problem 5
+function inchToCentimeter(inch) {
+    return inch * 2.54;
+}
+
+
+// Problem 6
+function poundToKilogram(pound) {
+    return pound * 0.453;
+}
+
+
+// Problem 7
+function yardToMeter(yard) {
+    return yard * 0.91;
+}
