e33bd06698e754b025786820840b164d888705dd
Ranbir <ranbir.webdev@gmail.com>
2026-06-05 10:58:51 +0600

    add chapter-13 of part-04

---
+++ chapter-13/part-04.js
@@ -0,0 +1,22 @@
+//problem-01
+const numbers = [50, 12, 25, 8, 15];
+numbers.sort((a,b) => a-b);
+console.log(numbers);
+
+
+//problem-02
+const marks = [ 13, 2, 45, 9, 6];
+marks.sort((a,b) => b - a);
+console.log(marks)
+
+
+//problem-03
+const friends = [{name: 'Ali', age:29}, {name:'Sara', age:22}, {name:'Tariq',  age:35}];
+friends.sort();
+console.log(friends)
+
+
+//problem-04
+const names = ['nabil', 'zubayer', 'sarwar','delwar'];
+names.sort();
+console.log(names);
