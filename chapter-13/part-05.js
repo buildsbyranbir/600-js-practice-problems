2a52e9c0535569fc4b854135da6e531595016b97
Ranbir <ranbir.webdev@gmail.com>
2026-06-06 10:41:26 +0600

    add chapter-05 of part-05

---
+++ chapter-13/part-05.js
@@ -0,0 +1,16 @@
+//problem-01
+const names = ['Zara', 'Tariq', 'Amir', 'Lina']
+names.reverse();
+console.log(names);
+
+
+//problem-02
+const numbers = [1, 4, 9, 7];
+numbers.reverse();
+console.log(numbers);
+
+
+//problem-03
+const users = [{name: 'Ali', age:30}, {name:'Sara', age:25}, {name:'Tariq', age:35}];
+users.reverse();
+console.log(users);
