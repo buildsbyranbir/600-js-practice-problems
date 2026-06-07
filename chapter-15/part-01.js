8f2861adfbb4b88f34d731ba54f34c7f8e86aa55
Ranbir <ranbir.webdev@gmail.com>
2026-06-07 13:59:26 +0600

    add chapter-14 of part-01

---
+++ chapter-15/part-01.js
@@ -0,0 +1,64 @@
+// Problem 1
+// city name er string banano
+const city = 'Chattogram';
+
+// g kon index e ache ber kora
+const index = city.indexOf('g');
+
+console.log(index);
+
+
+// Problem 2
+// division name er string banano
+const division = 'Sylhet';
+
+// y ache kina check kora
+const hasY = division.includes('y');
+
+console.log(hasY);
+
+
+
+
+// Problem 3
+// name string
+const name = 'Rifat';
+
+// shesh character ber kora
+const lastChar = name[name.length - 1];
+
+console.log(lastChar);
+
+
+
+// Problem 4
+// language string
+const language = 'javascript';
+
+// "rip" kon index theke shuru hoyeche check kora
+const index = language.indexOf('rip');
+
+console.log(index);
+
+
+
+// Problem 5
+// text string
+const text = 'Immutable';
+
+// string er length ber kora
+console.log(text.length);
+
+// mutable kina check kora
+// string sobsomoy immutable hoy
+console.log('String is immutable'); // String is immutable
+
+
+
+
+
+// indexOf() -> kon character ba word kon index e ache ber kore
+// includes() -> ache kina true/false return kore
+// length -> string er mot koyta character ache
+// string immutable -> string ke direct change kora jay na
+// array mutable -> array ke change kora jay
