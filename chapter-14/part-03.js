0de103f88158849d7b3f8e9ab77a99ae51301a33
Ranbir <ranbir.webdev@gmail.com>
2026-06-09 14:30:12 +0600

    add chapter-14 of part-03

---
+++ chapter-14/part-03.js
@@ -0,0 +1,41 @@
+// Problem 1
+// "Hi" string ke 10 bar repeat kore console e dekhano
+
+const greeting = "Hi";
+const repeatedGreeting = greeting.repeat(10);
+
+console.log(repeatedGreeting);
+
+// Output:
+// HiHiHiHiHiHiHiHiHiHi
+
+
+
+
+// Problem 2
+// Prothom "JavaScript" ke "JS" diye replace kora
+
+const sentence1 = "I am learning JavaScript. JavaScript is amazing!";
+
+const result1 = sentence1.replace("JavaScript", "JS");
+
+console.log(result1);
+
+// Output:
+// I am learning JS. JavaScript is amazing!
+
+
+
+
+
+// Problem 3
+// Sob "apples" ke "mangoes" diye replace kora
+
+const sentence2 = "I love apples and apples are tasty!";
+
+const result2 = sentence2.replaceAll("apples", "mangoes");
+
+console.log(result2);
+
+// Output:
+// I love mangoes and mangoes are tasty!
